0587d9eb0a6ee48946a1e4a580b2ee89a84f5a12
Cristian Guerra <cristian@digithaigroup.com>
2021-03-24 11:09:30 +0700

    Add resources and their parsing on Models

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -9,4 +9,50 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    var styleData: Data?
+    
+    init() {
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        
+        // Parse modules data
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            
+            let data = try Data(contentsOf: jsonUrl!)
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                
+                let moduleData = try decoder.decode([Module].self, from: data)
+                
+                self.modules = moduleData
+                
+            } catch {
+                print(error)
+            }
+            
+        } catch {
+            print(error)
+        }
+        
+        // Parse style data
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            
+            let data = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = data
+            
+        } catch {
+            print(error)
+        }
+    }
+        
 }
